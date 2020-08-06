0e774d7c350ebf1b70106cb3513d4fa7b75b5ac9
NellyCR <66942490+NellyCR@users.noreply.github.com>
2020-08-06 14:12:03 +0200

    Update W2 D2 SQL LAB 2 Challenge 1 v2.sql

--- module-1/Advanced-mySQL/W2 D2 SQL LAB 2 Challenge 1 v2.sql
+++ module-1/Advanced-mySQL/W2 D2 SQL LAB 2 Challenge 1 v2.sql
@@ -19,13 +19,53 @@ JOIN sales on titles.title_id = sales.title_id;
 
 # Step 2 : Aggregate the total royalties for each title and author .... I couldn't figure out how to SUM(sales_royalty)
 
+SELECT 
+authors.au_id,  
+titleauthor.title_id,
+SUM(titles.advance * titleauthor.royaltyper / 100) as "advance",
+SUM(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) as "total_royalty"
+
+FROM authors
+JOIN titleauthor on authors.au_id = titleauthor.au_id
+JOIN titles on titleauthor.title_id = titles.title_id 
+JOIN sales on titles.title_id = sales.title_id
+GROUP BY authors.au_id,titleauthor.title_id;
 
 # Step 3 : Calculate the total profits of each author
 
+SELECT 
+authors.au_id,  
+SUM(titles.advance * titleauthor.royaltyper / 100) + SUM(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100)  as total_profit
+
+FROM authors
+JOIN titleauthor on authors.au_id = titleauthor.au_id
+JOIN titles on titleauthor.title_id = titles.title_id 
+JOIN sales on titles.title_id = sales.title_id
+GROUP BY authors.au_id;
+
 # Step 4 :  create a permanent table named most_profiting_authors to hold the data about the most profiting authors. 
-# The table should have 2 columns: au_id - Author ID  & profits - The profits of the author aggregating the advances and royalties
 
+SELECT 
+authors.au_id,  
+SUM(titles.advance * titleauthor.royaltyper / 100) + SUM(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100)  as total_profit
 
+FROM authors
+JOIN titleauthor on authors.au_id = titleauthor.au_id
+JOIN titles on titleauthor.title_id = titles.title_id 
+JOIN sales on titles.title_id = sales.title_id
+GROUP BY authors.au_id;
+
+# Create a table with 2 columns: au_id - Author ID  & profits - The profits of the author aggregating the advances and royalties
+
+SELECT 
+authors.au_id,  
+SUM(titles.advance * titleauthor.royaltyper / 100) + SUM(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100)  as total_profit
+
+FROM authors
+JOIN titleauthor on authors.au_id = titleauthor.au_id
+JOIN titles on titleauthor.title_id = titles.title_id 
+JOIN sales on titles.title_id = sales.title_id
+GROUP BY authors.au_id;
 
 
 
