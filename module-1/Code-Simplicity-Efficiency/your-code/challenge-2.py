a1a20c72261d0c38900fc16a68fe6a979610adce
sarah-busson <65726929+sarah-busson@users.noreply.github.com>
2020-08-11 22:48:06 +0200

    Uploaded LAB for Code Simplicity Efficiency

--- module-1/Code-Simplicity-Efficiency/your-code/challenge-2.py
+++ module-1/Code-Simplicity-Efficiency/your-code/challenge-2.py
@@ -7,32 +7,29 @@ The code is functional but has a lot of room for improvement. Use what you have 
 about simple and efficient code, refactor the code.
 """
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
+import random
 
-def BatchStringGenerator(n, a=8, b=12):
-    r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
-        else:
-            import sys
-            sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
-    return r
+def RandomStringGenerator(length=12, generator=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
+    stop = 0
+    string = ''
+    while stop < length:
+        string += random.choice(generator)
+        stop += 1
+    return string
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
+def BatchStringGenerator(nb, mini=8, maxi=12):
+    batch = []
+    for i in range(nb):
+        if mini < maxi:
+            batch.append(RandomStringGenerator(random.choice(range(mini, maxi+1))))
+        elif mini == maxi:
+            batch.append(RandomStringGenerator(mini))
+    return batch
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+mini = input('Enter minimum string length: ')
+maxi = input('Enter maximum string length: ')
+while int(maxi) < int(mini):
+    maxi = input('Sorry, your maximum must be greater or equal to the minimum string length.\n Enter maximum string length: ')
+nb = input('How many random strings to generate? ')
+
+print(BatchStringGenerator(int(nb), int(mini), int(maxi)))
