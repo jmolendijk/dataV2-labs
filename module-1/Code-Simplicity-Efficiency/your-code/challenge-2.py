403a974f4bed8538ab383d391d655f4a442f0316
“leonvanderlee” <hello@leonvanderlee.com>
2020-08-11 12:10:20 +0200

    master

--- module-1/Code-Simplicity-Efficiency/your-code/challenge-2.py
+++ module-1/Code-Simplicity-Efficiency/your-code/challenge-2.py
@@ -7,7 +7,12 @@ The code is functional but has a lot of room for improvement. Use what you have 
 about simple and efficient code, refactor the code.
 """
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
+import random
+import string
+
+list1 = list(string.ascii_lowercase+string.digits)
+
+def RandomStringGenerator(l=12, a=list1):
     p = 0
     s = ''
     while p<l:
@@ -31,8 +36,8 @@ def BatchStringGenerator(n, a=8, b=12):
         r.append(RandomStringGenerator(c))
     return r
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
+minimum_input = input('Enter minimum string length: ')
+maximum_input = input('Enter maximum string length: ')
+strings_input = input('How many random strings to generate? ')
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+print(BatchStringGenerator(int(strings_input), int(minimum_input), int(maximum_input)))
