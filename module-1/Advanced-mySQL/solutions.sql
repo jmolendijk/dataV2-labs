2dc5225e6594fd756cd5bdf7449fcf85178388a6
SvetlanaGruzdeva <gruzdevasvetlana@gmail.com>
2020-10-20 15:51:36 +0200

    Submission of lab 2 W2D2

---
+++ module-1/Advanced-mySQL/solutions.sql
@@ -0,0 +1,78 @@
+-- CHALLENGE 1.
+-- Step 1: Calculate the royalty of each sale for each author and the advance for each author and publication
+select titles.title_id, 
+		authors.au_id, 
+        (titles.advance * (titleauthor.royaltyper/100)) as advance,
+        (titles.price * sales.qty * (titles.royalty/100) * (titleauthor.royaltyper/100)) as sales_royalty
+from titles join titleauthor	-- Excluding 1 book which doesn't have assigned author.
+on titles.title_id = titleauthor.title_id
+left join authors
+on titleauthor.au_id = authors.au_id
+left join sales
+on titles.title_id = sales.title_id;
+
+-- Step 2: Aggregate the total royalties for each title and author
+select calc.au_id, calc.title_id, sum(advance) as advance_total, sum(sales_royalty) as royalty_total
+from
+	(select titles.title_id, 
+			authors.au_id, 
+			(titles.advance * (titleauthor.royaltyper/100)) as advance,
+			(titles.price * sales.qty * (titles.royalty/100) * (titleauthor.royaltyper/100)) as sales_royalty
+	from titles join titleauthor	-- Excluding 1 book which doesn't have assigned author.
+	on titles.title_id = titleauthor.title_id
+	left join authors
+	on titleauthor.au_id = authors.au_id
+	left join sales
+	on titles.title_id = sales.title_id) as calc
+group by calc.title_id, calc.au_id;
+
+-- Step 3: Calculate the total profits of each author
+select profit.au_id, (profit.advance_total + profit.royalty_total) as profit
+from
+	(select calc.au_id, calc.title_id, sum(advance) as advance_total, sum(sales_royalty) as royalty_total
+	from
+		(select titles.title_id, 
+				authors.au_id, 
+				(titles.advance * (titleauthor.royaltyper/100)) as advance,
+				(titles.price * sales.qty * (titles.royalty/100) * (titleauthor.royaltyper/100)) as sales_royalty
+		from titles join titleauthor	-- Excluding 1 book which doesn't have assigned author.
+		on titles.title_id = titleauthor.title_id
+		left join authors
+		on titleauthor.au_id = authors.au_id
+		left join sales
+		on titles.title_id = sales.title_id) as calc
+		group by calc.title_id, calc.au_id) as profit
+order by profit desc
+limit 3;
+
+
+-- CHALLENGE 2.
+create temporary table publications.calc
+select titles.title_id, 
+		authors.au_id, 
+        (titles.advance * (titleauthor.royaltyper/100)) as advance,
+        (titles.price * sales.qty * (titles.royalty/100) * (titleauthor.royaltyper/100)) as sales_royalty
+from titles join titleauthor	-- Excluding 1 book which doesn't have assigned author.
+on titles.title_id = titleauthor.title_id
+left join authors
+on titleauthor.au_id = authors.au_id
+left join sales
+on titles.title_id = sales.title_id;
+
+create temporary table profit
+select calc.au_id, calc.title_id, sum(advance) as advance_total, sum(sales_royalty) as royalty_total
+from calc
+group by calc.title_id, calc.au_id;
+
+select profit.au_id, (profit.advance_total + profit.royalty_total) as profit
+from profit
+order by profit desc
+limit 3;
+
+
+-- CHALLENGE 3.
+create table most_profiting_authors_svetlana
+select profit.au_id as au_id, (profit.advance_total + profit.royalty_total) as profits
+from profit
+order by profits desc
+limit 3;
