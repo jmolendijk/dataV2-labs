0f2ea439b327fc14e68140d5c7d11e6aef283a16
Vijayalakshmi Kalidindi <iswetha522@gmail.com>
2020-10-20 22:23:01 +0200

    Completed remaining two tasks from the lab sessions

--- module-1/MySQL-Select/solutions.sql
+++ module-1/MySQL-Select/solutions.sql
@@ -16,3 +16,21 @@ on titles.title_id = titleauthor.title_id
 inner join publications.publishers publishers
 on publishers.pub_id = titles.pub_id
 group by authors.au_id, publishers.pub_name;
+
+select authors.au_id, authors.au_lname, authors.au_fname, count(titles.title_id) 
+from publications.authors authors
+left join publications.titleauthor titleauthor
+on authors.au_id = titleauthor.au_id
+left join publications.titles titles
+on titles.title_id = titleauthor.title_id
+group by authors.au_id
+order by count(titles.title_id) desc limit 3;
+
+select authors.au_id, authors.au_lname, authors.au_fname, count(titles.title_id) 
+from publications.authors authors
+left join publications.titleauthor titleauthor
+on authors.au_id = titleauthor.au_id
+left join publications.titles titles
+on titles.title_id = titleauthor.title_id
+group by authors.au_id
+order by count(titles.title_id) desc;
