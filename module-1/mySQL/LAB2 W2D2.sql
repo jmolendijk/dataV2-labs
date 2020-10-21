554c7416f632d54f040dadcc59f03c52f6375bd7
sebastieniron <sv.vandenbussche@hotmail.com>
2020-10-21 18:05:13 +0200

    Labs W2D3

--- module-1/mySQL/LAB2 W2D2.sql
+++ module-1/mySQL/LAB2 W2D2.sql
@@ -11,48 +11,42 @@ on titleauthor.title_id = sales.title_id
 order by sales_royalty DESC;
 
 /*STEP 2 */
-select authors.au_id, sales.title_id, sum(sales_royalty) AS total_royals, sum(advance) AS total_advance
+select authors.au_id, X.title_id, sum(sales_royalty) AS total_royals, advance
 	from (select authors.au_id, titleauthor.title_id, titles.advance*titleauthor.royaltyper/100 AS advance,
-	titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS sales_royalty
-	from authors
-	join titleauthor
-	on authors.au_id = titleauthor.au_id
-	join titles
-	on titleauthor.title_id = titles.title_id
-	join sales
-	on titleauthor.title_id = sales.title_id) as X,
-authors
+titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS sales_royalty
+from authors
 join titleauthor
 on authors.au_id = titleauthor.au_id
+join titles
+on titleauthor.title_id = titles.title_id
 join sales
-on sales.title_id = titleauthor.title_id
-group by authors.au_id, sales.title_id
-order by title_id desc;
+on titleauthor.title_id = sales.title_id) as X
+    join authors
+    on authors.au_id = X.au_id
+group by authors.au_id, X.title_id, advance;
 
 /*STEP 3 */
-select authors.au_id, sum(X.sales_royalty) + sum(X.advance) AS profit
-from (select authors.au_id, titleauthor.title_id, sum(X.sales_royalty) AS total_royals, sum(X.advance) AS total_advance
+select authors.au_id, sum(Y.total_royals + Y.advance) AS profit
+from (select authors.au_id, X.title_id, sum(sales_royalty) AS total_royals, advance
 	from (select authors.au_id, titleauthor.title_id, titles.advance*titleauthor.royaltyper/100 AS advance,
-	titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS sales_royalty
-	from authors
-	join titleauthor
-	on authors.au_id = titleauthor.au_id
-	join titles
-	on titleauthor.title_id = titles.title_id
-	join sales
-	on titleauthor.title_id = sales.title_id) as X,
-authors
+titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS sales_royalty
+from authors
 join titleauthor
 on authors.au_id = titleauthor.au_id
+join titles
+on titleauthor.title_id = titles.title_id
 join sales
-on titleauthor.title_id = sales.title_id
-group by authors.au_id, titleauthor.title_id) AS Y,
-authors
-join titleauthor
-on authors.au_id = titleauthor.au_id
+on titleauthor.title_id = sales.title_id) as X
+    join authors
+    on authors.au_id = X.au_id
+group by authors.au_id, X.title_id, advance) AS Y
+join authors
+on authors.au_id = Y.au_id
 group by authors.au_id
-order by profit desc
+order by profit DESC
 limit 3;
+
+
 
 
 
