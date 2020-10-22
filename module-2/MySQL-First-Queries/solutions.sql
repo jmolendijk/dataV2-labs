6855b5ded2e68872293a0ad7a26a6c6b777159be
Vijayalakshmi Kalidindi <iswetha522@gmail.com>
2020-10-22 13:54:48 +0200

    Completed left over tasks from lab sessions

--- module-2/MySQL-First-Queries/solutions.sql
+++ module-2/MySQL-First-Queries/solutions.sql
@@ -1,8 +1,24 @@
+# 1. What are the different genres?
 select distinct(prime_genre) from app;
+
+#2. Which is the genre with the most apps rated?
 select count(*), prime_genre from app where rating_count_tot > 0 group by prime_genre order by count(*) desc limit 1;
-select count(*), prime_genre from app where ((rating_count_tot > 0 ) or (rating_count_tot <= 0)) group by prime_genre order by count(*) desc limit 1;
-select count(*), prime_genre from app where ((rating_count_tot > 0) or (rating_count_tot <= 0)) group by prime_genre order by count(*) asc limit 1;
+
+#3. Which is the genre with most apps?
+select count(*), prime_genre from app where group by prime_genre order by count(*) desc limit 1;
+
+#4. Which is the one with least?
+select count(*), prime_genre from app where group by prime_genre order by count(*) asc limit 1;
+
+#5. Find the top 10 apps most rated.
 select track_name from app order by rating_count_tot desc limit 10;
+
+#6. Find the top 10 apps best rated by users.
 select track_name from app order by user_rating desc limit 10;
 
+#10. How could you take the top 3 regarding both user ratings and number of votes?
+select track_name, (rating_count_tot * user_rating) as combined_rating from app order by combined_rating desc limit 3;
+
+#11.Do people care about the price of an app? Do some queries, comment why are you doing them and the results you retrieve. What is your conclusion?
+select user_rating, avg(price) from app group by user_rating order by user_rating desc;
 
