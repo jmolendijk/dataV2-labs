772112848a375761609b6fa5a68fc31145a10d8d
Vijayalakshmi Kalidindi <iswetha522@gmail.com>
2020-10-28 00:03:38 +0100

    Completed Challenge 2 of code simplicity lab session

--- module-1/Code-Simplicity-Efficiency/your-code/challenge-2.py
+++ module-1/Code-Simplicity-Efficiency/your-code/challenge-2.py
@@ -6,33 +6,37 @@ the strings being generated.
 The code is functional but has a lot of room for improvement. Use what you have learned
 about simple and efficient code, refactor the code.
 """
+import random
+import sys
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
+default_length = 12
+default_character_list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','count','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']
+   
+min_len = int(input('Enter minimum string length: '))
+max_len = int(input('Enter maximum string length: '))
+rand_strings = int(input('How many random strings to generate? '))
 
-def BatchStringGenerator(n, a=8, b=12):
-    r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
+
+def RandomStringGenerator(length = default_length, character_set = default_character_list):
+
+    random_string = ''
+
+    for i in range(length):
+        random_string += random.choice(character_set)
+    
+    return random_string
+
+def BatchStringGenerator(count, min_length=8, max_length=12):
+    random_strings = []
+    for i in range(count):
+        random_length = None
+        if min_length < max_length:
+            random_length = random.choice(range(min_length, max_length))
+        elif min_length == max_length:
+            random_length = min_length
         else:
-            import sys
             sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
-    return r
+        random_strings.append(RandomStringGenerator(random_length))
+    return random_strings
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
-
-print(BatchStringGenerator(int(n), int(a), int(b)))
+print(BatchStringGenerator(rand_strings, min_len, max_len))
