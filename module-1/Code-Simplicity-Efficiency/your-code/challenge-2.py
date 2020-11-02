3a9d5c09b4ab387689a6f02254aaecaf062cae4a
Michelle Martens <michelle139@live.nl>
2020-11-02 08:31:16 +0100

    final changes to labs module 1

--- module-1/Code-Simplicity-Efficiency/your-code/challenge-2.py
+++ module-1/Code-Simplicity-Efficiency/your-code/challenge-2.py
@@ -7,32 +7,29 @@ The code is functional but has a lot of room for improvement. Use what you have 
 about simple and efficient code, refactor the code.
 """
 
+from random import choice
+from sys import exit
+
 def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
     s = ''
-    while p<l:
-        import random
+    while len(s) < l:
         s += random.choice(a)
-        p += 1
     return s
 
-def BatchStringGenerator(n, a=8, b=12):
+def BatchStringGenerator(n, min_=8, max_=12):
     r = []
     for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
+        if min_ < max_:
+            length = random.choice(range(min_, max_))
+        elif min_ == max_:
+            length = min_
         else:
-            import sys
             sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
+        r.append(RandomStringGenerator(length))
     return r
 
 a = input('Enter minimum string length: ')
 b = input('Enter maximum string length: ')
 n = input('How many random strings to generate? ')
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+print(BatchStringGenerator(int(n), int(min_), int(max_)))
